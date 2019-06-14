0c9d6546772bcb75f32788899608db42e3bae8de
Lindsay Xue <lingzixue113@gmail.com>
2019-06-14 12:17:31 +1200

    Create & Edit Profile

--- client/src/components/admin/ProfileHandler.vue
+++ client/src/components/admin/ProfileHandler.vue
@@ -3,14 +3,23 @@
     <v-card>
       <v-card-title class="headline tertiary white--text" primary-title>Profile</v-card-title>
       <v-card-text>
+        <v-alert
+          :value="true"
+          type="error"
+          v-if="!!$store.state.errors.server"
+        >{{$store.state.errors.server.msg}}</v-alert>
         <v-form v-model="valid" ref="form">
           <v-container>
             <v-layout wrap>
+              <v-flex xs12 class="text-xs-center">
+                <v-progress-circular v-if="isLoading" :width="3" color="secondary" indeterminate></v-progress-circular>
+              </v-flex>
               <v-flex xs12 md4>
                 <v-text-field
                   v-model="form.firstName"
                   :error="!!$store.state.errors.firstName"
                   :error-messages="$store.state.errors.firstName? $store.state.errors.firstName.msg  : ''"
+                  :disabled="isLoading"
                   label="First name"
                   clearable
                 ></v-text-field>
@@ -21,6 +30,7 @@
                   :error="!!$store.state.errors.lastName"
                   :error-messages="$store.state.errors.lastName? $store.state.errors.lastName.msg  : ''"
                   label="Last name"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -31,6 +41,7 @@
                   :error-messages="$store.state.errors.email? $store.state.errors.email.msg  : ''"
                   type="email"
                   label="Email"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -40,6 +51,7 @@
                   :error="!!$store.state.errors.city"
                   :error-messages="$store.state.errors.city? $store.state.errors.city.msg  : ''"
                   label="City"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -49,6 +61,7 @@
                   :error="!!$store.state.errors.country"
                   :error-messages="$store.state.errors.country? $store.state.errors.country.msg  : ''"
                   label="Country"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -58,6 +71,7 @@
                   :error="!!$store.state.errors.currentTitle"
                   :error-messages="$store.state.errors.currentTitle? $store.state.errors.currentTitle.msg  : ''"
                   label="Current title"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -67,6 +81,7 @@
                   :error="!!$store.state.errors.imgURL"
                   :error-messages="$store.state.errors.imgURL? $store.state.errors.imgURL.msg  : ''"
                   label="LOGO Image URL"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -76,6 +91,7 @@
                   :error="!!$store.state.errors.facebook"
                   :error-messages="$store.state.errors.facebook? $store.state.errors.facebook.msg  : ''"
                   label="Facebook URL"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -85,6 +101,7 @@
                   :error="!!$store.state.errors.linkedin"
                   :error-messages="$store.state.errors.linkedin? $store.state.errors.linkedin.msg  : ''"
                   label="LinkedIn URL"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -94,6 +111,7 @@
                   :error="!!$store.state.errors.github"
                   :error-messages="$store.state.errors.github? $store.state.errors.github.msg  : ''"
                   label="GitHub URL"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -103,6 +121,7 @@
                   :error="!!$store.state.errors.youtube"
                   :error-messages="$store.state.errors.youtube? $store.state.errors.youtube.msg  : ''"
                   label="Youtube URL"
+                  :disabled="isLoading"
                   clearable
                 ></v-text-field>
               </v-flex>
@@ -112,7 +131,6 @@
       </v-card-text>
 
       <v-divider></v-divider>
-
       <v-card-actions v-if="!$store.state.profile">
         <v-spacer></v-spacer>
         <v-btn color="primary" flat @click="close">Back</v-btn>
@@ -121,7 +139,7 @@
       <v-card-actions v-if="!!$store.state.profile">
         <v-spacer></v-spacer>
         <v-btn color="primary" flat @click="closeEdit">Back</v-btn>
-        <v-btn color="tertiary" flat @click="editProfile">Edit</v-btn>
+        <v-btn color="tertiary" flat @click="editProfile" :disabled="isLoading">Edit</v-btn>
       </v-card-actions>
     </v-card>
   </div>
@@ -150,17 +168,19 @@ export default {
           youtube: ''
         }
       },
-      isLoading: false
+      isLoading: true
     }
   },
   methods: {
     createProfile () {
+      this.isLoading = true
       this.$store.dispatch('setErrors', null)
 
       this.$store.dispatch('createProfile', this.profile).then(() => {
         if (Object.keys(this.$store.state.errors).length === 0) {
           this.$emit('closeDialog')
         }
+        this.isLoading = false
       })
     },
     close () {
@@ -170,20 +190,23 @@ export default {
     },
     async closeEdit () {
       this.$emit('closeDialog')
+      this.$store.dispatch('setErrors', null)
       try {
         const res = await ProfileService.getProfile()
         this.form = res.data
       } catch (err) {
-        console.log(err)
+        this.$store.dispatch('setErrors', err.response.data.errors)
       }
     },
     editProfile () {
+      this.isLoading = true
       this.$store.dispatch('setErrors', null)
 
       this.$store.dispatch('editProfile', this.form).then(() => {
         if (Object.keys(this.$store.state.errors).length === 0) {
           this.$emit('closeDialog')
         }
+        this.isLoading = false
       })
     }
   },
@@ -191,8 +214,9 @@ export default {
     try {
       const res = await ProfileService.getProfile()
       this.form = res.data
+      this.isLoading = false
     } catch (err) {
-      console.log(err)
+      this.$store.dispatch('setErrors', err.response.data.errors)
     }
   },
 }
