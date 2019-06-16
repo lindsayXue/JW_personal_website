675ea1eba7c7f11c1ed2a1594b99a4394829a4c7
Lindsay Xue <lingzixue113@gmail.com>
2019-06-17 11:14:56 +1200

    Project list debug

--- client/src/components/common/PojectsList.vue
+++ client/src/components/common/PojectsList.vue
@@ -11,64 +11,66 @@
         <ProjectHandler v-on:closeDialog="closeDialog"/>
       </v-dialog>
     </h1>
-    <div class="list-content">
+    <v-layout class="list-content" row wrap justify-center>
       <v-alert
         :value="true"
         type="error"
         class="alert my-4"
         v-if="$store.state.isAdmin && (!projectsData || projectsData.length === 0)"
       >No data yet!</v-alert>
-      <v-dialog v-model="dialogProject" width="600">
-        <template v-slot:activator="{ on }">
-          <v-card
-            class="project-card mb-4"
-            v-for="item in projectsData"
-            :key="item._id"
-            hover
-            v-on="on"
-            @click="reviewProject(item)"
-          >
-            <v-btn
-              fab
-              flat
-              dark
-              small
-              color="tertiary"
-              @click="deleteItem(item._id)"
-              absolute
-              right
-              v-if="$store.state.isAdmin"
+      <v-flex md9 xs11>
+        <v-dialog v-model="dialogProject" width="600">
+          <template v-slot:activator="{ on }">
+            <v-card
+              class="project-card mb-4"
+              v-for="item in projectsData"
+              :key="item._id"
+              hover
+              v-on="on"
+              @click="reviewProject(item)"
             >
-              <i class="fas fa-times"></i>
-            </v-btn>
-            <v-layout wrap>
-              <v-flex xs4 class="hidden-sm-and-down">
-                <v-img class="secondary" height="200" width="200" :src="item.imgURL"></v-img>
-              </v-flex>
-              <v-flex sm7 xs11>
-                <v-card-title class="headline tertiary--text">{{item.name}}</v-card-title>
-                <v-card-text class="detail" v-html="item.detail.slice(0, 100) + '...'"></v-card-text>
-              </v-flex>
-            </v-layout>
+              <v-btn
+                fab
+                flat
+                dark
+                small
+                color="tertiary"
+                @click="deleteItem(item._id)"
+                absolute
+                right
+                v-if="$store.state.isAdmin"
+              >
+                <i class="fas fa-times"></i>
+              </v-btn>
+              <v-layout wrap>
+                <v-flex xs4 class="hidden-sm-and-down">
+                  <v-img class="secondary" height="200" width="200" :src="item.imgURL"></v-img>
+                </v-flex>
+                <v-flex sm7 xs11>
+                  <v-card-title class="headline tertiary--text">{{item.name}}</v-card-title>
+                  <v-card-text class="detail" v-html="item.detail.slice(0, 100) + '...'"></v-card-text>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </template>
+          <v-card class="project-card">
+            <v-card-title
+              class="headline tertiary--text"
+              primary-title
+            >{{selectProject? selectProject.name : ''}}</v-card-title>
+
+            <v-card-text class="detail" v-html="selectProject? selectProject.detail:''"></v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" flat @click="dialogProject = false">Close</v-btn>
+            </v-card-actions>
           </v-card>
-        </template>
-        <v-card class="project-card">
-          <v-card-title
-            class="headline tertiary--text"
-            primary-title
-          >{{selectProject? selectProject.name : ''}}</v-card-title>
-
-          <v-card-text class="detail" v-html="selectProject? selectProject.detail:''"></v-card-text>
-
-          <v-divider></v-divider>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" flat @click="dialogProject = false">Close</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </div>
+        </v-dialog>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 <script>
